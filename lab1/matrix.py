9692b61317c46889d43a8b68ab5d57ceddd884e3
Yauheni Shaikouski <evgenyshaikovskiy@gmail.com>
2022-10-22 09:39:24 +0300

    fix weights

---
+++ lab1/matrix.py
@@ -0,0 +1,46 @@
+import unittest
+import numpy as np
+
+
+def m_dot(X, Y):
+    return [[sum(a * b for a, b in zip(X_ROW, Y_COL)) for Y_COL in zip(*Y)] for X_ROW in X]
+
+
+def m_t(M):
+    return [[M[j][i] for j in range(len(M))] for i in range(len(M[0]))]
+# print(Y)
+
+#print(np.dot(X, Y))
+#print(m_dot(X, Y))
+
+
+class m_test(unittest.TestCase):
+    def __init__(self, X, Y):
+        self.X = X
+        self.Y = Y
+        super().__init__()
+
+    def setUp(self):
+        pass
+
+    def test_mul(self):
+        self.assertEqual(np.dot(self.X, self.Y), m_dot(self.X, self.Y))
+
+    def test_x(self):
+        self.assertEqual(self.X.T, m_t(self.X))
+
+    def test_y(self):
+        self.assertEqual(self.X.T, m_t(self.X))
+
+
+if __name__ == 'main':
+    arr = np.arange(50)
+    X = arr.reshape(10, 5)
+    #print(X)
+    arr = np.arange(100)
+    Y = arr.reshape(5, 20)
+    
+    
+    print(np.dot(X, Y), m_dot(X, Y))
+    print(X.T == m_t(X))
+    print(Y.T == m_t(Y))
